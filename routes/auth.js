3af1df99288d0721482c120f3ecd9795267caee2
Maria Juravic <mariajuravic@yahoo.com>
2023-01-10 10:48:50 -0800

    complete auth routes

--- routes/auth.js
+++ routes/auth.js
@@ -3,13 +3,29 @@
 const Router = require("express").Router;
 const router = new Router();
 const User = require("../models/user");
-const { SECRET_KEY } = require("../config");
+const { SECRET_KEY, BCRYPT_WORK_FACTOR } = require("../config");
 const jwt = require("jsonwebtoken");
 
 
 const { BadRequestError } = require("../expressError");
 
 /** POST /login: {username, password} => {token} */
+
+router.post("/login", async function(req, res, next) {
+  if(req.body === undefined) throw new BadRequestError("Body must be included.");
+
+  const { username, password } = req.body;
+  const result = await User.authenticate(username, password);
+
+
+  if(result === true) {
+    const payload = {username};
+    const token = jwt.sign(payload, SECRET_KEY);
+    return res.status(201).json({token});
+  }
+  return res.json({"error": "Username or password is invalid."})
+})
+
 
 
 /** POST /register: registers, logs in, and returns token.
