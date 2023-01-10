2c4b29bf60963586458ffd88d5b05353390343ca
Daniel Zeljko <danielzeljko@users.noreply.github.com>
2023-01-09 18:33:15 -0800

    WIP register route

--- routes/auth.js
+++ routes/auth.js
@@ -2,6 +2,12 @@
 
 const Router = require("express").Router;
 const router = new Router();
+const User = require("../models/user");
+const { SECRET_KEY } = require("../config");
+const jwt = require("jsonwebtoken");
+
+
+const { BadRequestError } = require("../expressError");
 
 /** POST /login: {username, password} => {token} */
 
@@ -10,5 +16,29 @@ const router = new Router();
  *
  * {username, password, first_name, last_name, phone} => {token}.
  */
+
+router.post("/register", async function (req, res, next) {
+  // TODO: Make middleware to check for empty body make sure all
+  // needed fields are included
+
+  // TODO: Try catch for duplicate username
+
+  // TODO: What do they mean by logs in?? set 'res.locals.user?'
+
+  if (req.body === undefined) throw new BadRequestError("Body must be included.");
+
+  const { username, password, first_name, last_name, phone } = req.body;
+  const result = await User.register({username, password, first_name, last_name, phone});
+  const authenticateUser = User.authenticate(result.username, password);
+
+  let token;
+  if (authenticateUser) {
+    const payload = {"username": result.username}
+    token = jwt.sign(payload, SECRET_KEY);
+  }
+
+  return res.status(201).json({token});
+
+});
 
 module.exports = router;
